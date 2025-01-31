d273e9d51a6cf88ef3400cef56040579cef725ab
krapivkaGIT <anatoliy.krapivka@gmail.com>
2025-01-31 15:15:20 +0200

    update
    update upload

---
+++ app-files/data.js
@@ -0,0 +1,131 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-img_6300",
+      "name": "IMG_6300",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.014875078788449514,
+        "pitch": 0.14532714733646657,
+        "fov": 1.3918337361947004
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.06809602484121413,
+          "pitch": 0.777778558987972,
+          "rotation": 0,
+          "target": "1-img_6299-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.24319747972255712,
+          "pitch": 0.5556840921420623,
+          "title": "цікава інформація&nbsp;",
+          "text": "<span style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">цікава інформація&nbsp;&nbsp;<br><br>&nbsp;тут це для того&nbsp;<br>&nbsp;шоб&nbsp;<br>це&nbsp;<br>&nbsp;ше&nbsp; ще ну&nbsp;<br>&nbsp;якби&nbsp; то воно&nbsp;<br>&nbsp;тогощо як&nbsp; то кажуть не для всіх&nbsp;</span>"
+        }
+      ]
+    },
+    {
+      "id": "1-img_6299-2",
+      "name": "IMG_6299 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3807823883401902,
+          "pitch": 0.7776339278725484,
+          "rotation": 0,
+          "target": "2-img_6301"
+        },
+        {
+          "yaw": -2.5553345616057292,
+          "pitch": 0.48905489548839753,
+          "rotation": 7.0685834705770345,
+          "target": "0-img_6300"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-img_6301",
+      "name": "IMG_6301",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
